5df9542abc524c34c1492db25c5062753efdd66f
Jeremy Cantu <jcantu521@gmail.com>
2017-06-01 12:17:31 -0500

    Added tests

--- Raw Files/git-addit
+++ Raw Files/git-addit
@@ -4,5 +4,8 @@
 #
 # quick add + commit
 
+message=$1
+test -z $message && echo "ERROR: message required" 1>&2 && exit 1
+
 git add -A
-git commit -m "$1"
+git commit -m "$message"
--- Shell Scripts/git-get-fork.sh
+++ Shell Scripts/git-get-fork.sh
@@ -5,6 +5,8 @@
 # quick grab of upstream fork and subsequent merge
 
 url=$1
+test -z $url && echo "ERROR: fork URL required" 1>&2 && exit 1
+
 git remote add upstream $url
 git fetch upstream
 git merge upstream/master
