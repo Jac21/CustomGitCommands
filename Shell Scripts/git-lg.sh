fc665a7898df95a593b3e2fc9fc2319c65d01d2c
Jeremy Cantu <jcantu521@gmail.com>
2017-07-25 14:47:02 -0500

    Added formatted log alias

---
+++ Shell Scripts/git-lg.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# git-lg
+#
+# log in a nicely formatted manner
+
+git log --color --pretty=format:'%C(auto)%h %Cred %<(10,trunc)%an %Creset%C(auto)%s %Cgreen(%cr,%ar) %Creset%C(auto)%d'
